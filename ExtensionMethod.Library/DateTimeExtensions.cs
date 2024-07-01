a868fed24a78118de0de022c88bf44a727b0534f
Hamid Mughal <mughal.hamid@gmail.com>
2024-07-01 14:46:46 +0500

    Added Humanzer extension

---
+++ ExtensionMethod.Library/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod.Library
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToFriendlyString(this DateTime dateTime)
+        {
+            return dateTime.Humanize();
+        }
+    }
+}
---
+++ ExtensionMethod.Library/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod.Library
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string @string)
+        {
+            return @string.Titleize();
+        }
+    }
+}
